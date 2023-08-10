4fbf98e120d76eb15447f4467843924856fa6931
QuerulousEggman <ultimofantasista10@gmail.com>
2023-08-10 16:57:42 +0100

    changes 10-08-23

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const form = document.querySelector("form");
 
 form.addEventListener("Submit", function (event) {
   event.preventDefault();
+  const place = document.querySelector("input#place");
+  const like_to_see = document.querySelector("input#like_to_see");
+  const stay = document.querySelector("input#stay");
   const dob = document.querySelector("input#dob");
+  const gender = document.querySelector("input#gender");
+  const occupation = document.querySelector("input#occupation");
   const formButton = document.querySelector("button");
-  formButton.textContent = console.log(dob.value);
+  formButton.textContent = console.log{
+    place: place.value,
+    like_to_see: like_to_see.value,
+    stay: stay.value,
+    dob: dob.value,
+    gender: gender.value,
+    occupation: occupation.value,
+  };
 })
